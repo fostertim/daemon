62ac7a94e794e09b2bcb5deeecb7912ad4b78ae2
Tim Foster <fostertim4@gmail.com>
2015-11-21 18:48:25 -0800

    integrated hibike support

--- runtime/api/Robot.py
+++ runtime/api/Robot.py
@@ -1,24 +1,31 @@
 # ------
-# Robot.py class 
+# Robot.py class
 # This runs the robot.
 # Copyright 2015. Pioneers in Engineering.
 # ------
 from grizzly import *
 #import hibike.hibike as sensors
+from Gamepads import *
 #import Motors
+
 
 motor = {}
 name_to_grizzly = {}
-
-
+runtime_ansible = None
+allData = {} 
 
 def init():
     addrs = Grizzly.get_all_ids()
     addrs = Grizzly.get_all_ids()
+#    runtime_ansible = Ansible('runtime')
 
-    # Brute force to find all 
+    #h = hibike.Hibike()
+    #connectedDevices = h.getEnumeratedDevices() #get list of devices
+    #h.subscribeToDevices(connectedDevices) #subscribe to devices
+
+    # Brute force to find all
     for index in range(len(addrs)):
-        # default name for motors is motor0, motor1, motor2, etc 
+        # default name for motors is motor0, motor1, motor2, etc
         grizzly_motor = Grizzly(addrs[index])
         grizzly_motor.set_mode(ControlMode.NO_PID, DriveMode.DRIVE_COAST)
         grizzly_motor.limit_acceleration(142)
@@ -28,21 +35,33 @@ def init():
         name_to_grizzly['motor' + str(index)] = grizzly_motor
         #motor['motor' + str(index)] = grizzly_motor.get_target()
 
-    print(name_to_grizzly, addrs)
+    #print(name_to_grizzly, addrs)
 
 def get_motor(name):
     #return motor[name]
     return None
 
 def set_motor(name, value):
-    print(name_to_grizzly)
-    grizzly = name_to_grizzly[name]    
+    #print(name_to_grizzly)
+    grizzly = name_to_grizzly[name]
     grizzly.set_target(value)
     #motor[name] = value
 
 # TODO: implement
+
+
+def update():
+    command =student_ansible.recv()
+    if command:
+	msg_type, content = command['header']['msg_type'], command['content']
+	if msg_type == "sensor_value":
+            allData = content 
+            print(allData)
+
 def get_sensor(name):
-    return None
+    if name in allData:
+        return allData[name]
+    return 'Error'#searched list
 
 def get_all_motors():
     return motors
--- runtime/runtime.py
+++ runtime/runtime.py
@@ -2,10 +2,13 @@ import subprocess, signal, sys
 from ansible import Ansible
 import threading
 import time
+
 from grizzly import *
 #from api import Robot
 #from api import Gamepads
-
+from api import hibike
+from api import deviceContext
+import time
 
 dawn_ansible = Ansible('dawn')
 runtime_ansible = Ansible('runtime')
@@ -27,7 +30,6 @@ runtime_ansible = Ansible('runtime')
 #peripheral_thread.start()
 
 running_code = False
-
 
 pobs = set() # set of all active processes
 pobslock = threading.Lock()  # Ensures that only one processs modifies pobs at a time
@@ -53,11 +55,23 @@ def p_watch(p):
     with pobslock:
         pobs.remove(p)
 
+context = deviceContext.DeviceContext()
+h = hibike.Hibike(context)
+connectedDevices = h.getEnumeratedDevices()
+context.subToDevices(connectedDevices)
+
+def get_all_data(connectedDevices):
+
+    all_data = {}
+    for t in connectedDevices:
+        all_data[t[0]] = context.getData(t[0],"dataUpdate")
+#    print(all_data)
+    return all_data
 
 while True:
-    command = dawn_ansible.recv() 
+    command = dawn_ansible.recv()
     if command:
-        print("Message received from ansible! " + command['header']['msg_type'])
+        #print("Message received from ansible! " + command['header']['msg_type'])
         msg_type, content = command['header']['msg_type'], command['content']
         if msg_type == 'execute':
 	    print("Ansible said to start the code")
@@ -75,7 +89,7 @@ while True:
             if running_code:
                 with pobslock:
                     print("killed")
-                    for p in pobs: 
+                    for p in pobs:
                         p.terminate() #ideal way to shut down
                         #p.kill()
                     pobs.clear()
@@ -86,3 +100,5 @@ while True:
                 running_code = False
         elif msg_type == 'gamepad':
             runtime_ansible.send(command)
+        runtime_ansible.send_message("sensor_value",get_all_data(connectedDevices))
+
